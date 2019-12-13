fd052e0ea7b41eac989f00c002a2eba791e1b9a4
Claire <clairebun_3@hotmail.com>
2019-12-13 16:17:07 -0500

    fix linting

--- app/src/components/index.jsx
+++ app/src/components/index.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import { Box } from "@material-ui/core"
 import FileUpload from "./file-upload"
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     with: "100%",
     display: "flex",
