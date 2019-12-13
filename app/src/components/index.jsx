3637b50582c90a983f52cf0180b586915b75eb51
Claire <clairebun_3@hotmail.com>
2019-12-13 16:16:16 -0500

    text responsive

--- app/src/components/index.jsx
+++ app/src/components/index.jsx
@@ -3,13 +3,18 @@ import { makeStyles } from "@material-ui/core/styles"
 import { Box } from "@material-ui/core"
 import FileUpload from "./file-upload"
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     with: "100%",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center"
+  },
+  text: {
+    justifyContent: "center",
+    margin: "auto",
+    padding: "2vh"
   }
 }))
 
@@ -17,17 +22,20 @@ const Index = () => {
   const classes = useStyles()
   return (
     <div className={classes.root}>
-      <p>
-        This website sole purpose is to convert docker compose files into Kubernetes
-        items.
-        <br />
-        You can convert your docker-compose files into three simple steps:
-      </p>
-      <ol>
-        <li>Click on the &quot;Choose File&quot; button.</li>
-        <li>Select your docker-compose.yml file.</li>
-        <li>Press submit to get your compose to kubernetes file.</li>
-      </ol>
+      <Box className={classes.text}>
+        <p>
+          This website sole purpose is to convert docker compose files into
+          Kubernetes items.
+          <br />
+          You can convert your docker-compose files into three simple steps:
+        </p>
+        <ol>
+          <li>Click on the &quot;Choose File&quot; button.</li>
+          <li>Select your docker-compose.yml file.</li>
+          <li>Press submit to get your compose to kubernetes file.</li>
+        </ol>
+      </Box>
+
       <Box m="2rem">
         <FileUpload />
       </Box>
