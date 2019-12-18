e2c90e6408e25400a636d9f55ed88e4cfc4eb511
Gabriel Jean <gabriel@Gabriels-MacBook-Pro.local>
2019-12-17 19:55:31 -0500

    Added error logging in Upload route

--- backend/routes/Upload.js
+++ backend/routes/Upload.js
@@ -6,6 +6,11 @@ var upload = multer({ dest: 'uploads/' });
 router.post('/', upload.single('compose_file'), function (req, res, next) {
   const filename = req.file.originalname;
   res.json({ status: "ok", filename: filename, });
-  })
+  });
 
+router.use(function (err, req, res, next) {
+  console.log('This is the invalid field ->', err.field);
+  next(err);
+  });
+  
 module.exports = router;
