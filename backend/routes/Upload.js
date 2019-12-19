b4cd47672e687f4afe35dc55777ec9b433a97d68
Claire <clairebun_3@hotmail.com>
2019-12-18 23:30:36 -0500

    add prettier and fix lint

--- backend/routes/Upload.js
+++ backend/routes/Upload.js
@@ -5,13 +5,14 @@ const multer = require('multer');
 
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/', upload.single('compose_file'), (req, res, next) => {
+router.post('/', upload.single('compose_file'), (req, res) => {
   const filename = req.file.originalname;
   // let data = fs.createReadStream(req.files.test.path,'utf8');
   res.json({ status: 'ok', filename });
 });
 
 router.use((err, req, res, next) => {
+  // eslint-disable-next-line no-console
   console.log('This is the invalid field ->', err.field);
   next(err);
 });
