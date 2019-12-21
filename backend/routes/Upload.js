3688711041fb90c04f8c1df0e5f1ce8878b1fffe
Gabriel Jean <theelithe555@gmail.com>
2019-12-21 20:23:20 +0000

    code cleaning

--- backend/routes/Upload.js
+++ backend/routes/Upload.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const fs = require('fs');
+
 const router = express.Router();
 const multer = require('multer');
+
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/', upload.single('compose_file'), (req, res) => {
   const filename = req.file.originalname;
-  const path = req.file.path;
-  console.log("path : " + path);
-  fs.readFile(path, 'utf8', function(err, contents) {
-    console.log(contents);
-    res.json({ status: 'ok', filename: filename, content: contents });
+  const { path } = req.file;
+  fs.readFile(path, 'utf8', (err, contents) => {
+    res.json({ status: 'ok', filename, content: contents });
   });
 });
 
-
-
-
-console.log('after calling readFile');
 // router.use((err, req, res, next) => {
 //   // eslint-disable-next-line no-console
 //   console.log('This is the invalid field ->', err.field);
