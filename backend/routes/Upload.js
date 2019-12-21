b3cd8619525fe21d66a08dd4848554b217fdb84e
Gabriel Jean <theelithe555@gmail.com>
2019-12-21 19:55:45 +0000

    Read file using asynchronous function

--- backend/routes/Upload.js
+++ backend/routes/Upload.js
@@ -6,11 +6,18 @@ const upload = multer({ dest: 'uploads/' });
 
 router.post('/', upload.single('compose_file'), (req, res) => {
   const filename = req.file.originalname;
-  data = fs.readFileSync(req.file.path,'utf8');
-  console.log("data : " + data);
-  res.json({ status: 'ok', filename: filename, content: data });
+  const path = req.file.path;
+  console.log("path : " + path);
+  fs.readFile(path, 'utf8', function(err, contents) {
+    console.log(contents);
+    res.json({ status: 'ok', filename: filename, content: contents });
+  });
 });
 
+
+
+
+console.log('after calling readFile');
 // router.use((err, req, res, next) => {
 //   // eslint-disable-next-line no-console
 //   console.log('This is the invalid field ->', err.field);
