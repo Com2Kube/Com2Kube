91a3f3db40d2ffa3c7a6cd5103c5216ca511466d
Gabriel Jean <theelithe555@gmail.com>
2019-12-23 18:42:47 +0000

    Conversion Working

--- backend/routes/Upload.js
+++ backend/routes/Upload.js
@@ -5,8 +5,6 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const { exec } = require('child_process');
 
-
-
 // router.post('/', upload.single('compose_file'), (req, res) => {
 //   const filename = req.file.originalname;
 //   path = req.file.path;
@@ -19,24 +17,30 @@ const { exec } = require('child_process');
 
 router.post('/', upload.single('compose_file'), (req, res) => {
   var path = req.file.path;
-  exec('kompose convert -f ' + path + " --out results", (err, stdout, stderr) => {
-    if (err) {
-      // node couldn't execute the command
-      return;
-    }
-  
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-    fs.readFile(path, 'utf8', function(err, contents) {
-    console.log(contents);
-    res.json({ content: contents });
-  });
-  });
+  convert(path)
 });
 
+function readfile(path){
+  fs.readFile(path, 'utf8', function(err, contents) {
+    // console.log(contents);
+  });
+}
 
+function convert(path) {
+  exec(
+    'kompose convert -f ' + path + ' --stdout',
+    (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        return;
+      }
 
+      // the *entire* stdout and stderr (buffered)
+      data = `stdout: ${stdout}`
+      errors = `stderr: ${stderr}`
+    }
+  );
+}
 // router.use((err, req, res, next) => {
 //   // eslint-disable-next-line no-console
 //   console.log('This is the invalid field ->', err.field);
