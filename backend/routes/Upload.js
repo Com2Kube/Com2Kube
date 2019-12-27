9b73586a522a87e269ca7ea01212014d02671e09
Gabriel Jean <theelithe555@gmail.com>
2019-12-27 22:58:29 +0000

    backend is sending result as response

--- backend/routes/Upload.js
+++ backend/routes/Upload.js
@@ -1,46 +1,34 @@
 const express = require('express');
 const fs = require('fs');
+
 const router = express.Router();
 const multer = require('multer');
+
 const upload = multer({ dest: 'uploads/' });
 const { exec } = require('child_process');
 
-// router.post('/', upload.single('compose_file'), (req, res) => {
-//   const filename = req.file.originalname;
-//   path = req.file.path;
-//   console.log("path : " + path);
-//   fs.readFile(path, 'utf8', function(err, contents) {
-//     console.log(contents);
-//     res.json({ status: 'ok', filename: filename, content: contents });
-//   });
-// });
 
-router.post('/', upload.single('compose_file'), (req, res) => {
-  var path = req.file.path;
-  convert(path)
-});
-
-function readfile(path){
-  fs.readFile(path, 'utf8', function(err, contents) {
-    // console.log(contents);
+function convert(path, callback) {
+  exec(`kompose convert -f ${path} --stdout`, (err, stdout, stderr) => {
+    if (err) {
+      console.log("An error has occurred. Abort everything!");
+      return callback(err);
+    }
+    callback(err,stdout);
   });
 }
 
-function convert(path) {
-  exec(
-    'kompose convert -f ' + path + ' --stdout',
-    (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        return;
-      }
-
-      // the *entire* stdout and stderr (buffered)
-      data = `stdout: ${stdout}`
-      errors = `stderr: ${stderr}`
+router.post('/', upload.single('compose_file'), (req, res) => {
+  const { path } = req.file;
+  convert(path, function(err, data) {
+    if (err){
+      console.log(err);
     }
-  );
-}
+    console.log(data);
+    res.json({data: data})
+  });
+});
+
 // router.use((err, req, res, next) => {
 //   // eslint-disable-next-line no-console
 //   console.log('This is the invalid field ->', err.field);
