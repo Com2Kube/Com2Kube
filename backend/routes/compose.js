53ef784c0cb444f39e7258d9c884b4303290931a
Gabriel Jean <theelithe555@gmail.com>
2020-01-12 15:28:11 +0000

    added max file size validation

--- backend/routes/compose.js
+++ backend/routes/compose.js
@@ -15,15 +15,14 @@ function komposeConvert(path, callback) {
 router.post('/', upload.single('compose_file'), (req, res) => {
   // Get file path
   const { path } = req.file;
-  console.log(req.file.size)
   // Get file extention
   const ext = req.file.originalname.toLocaleLowerCase().split('.');
 
   // Check if file extention is valid
   if (ext[ext.length - 1] === 'yml' || ext[ext.length - 1] === 'yaml') {
     // Check if file is empty
-    if (req.file.size != 0){
-          // Call function to initate file conversion
+    if (req.file.size != 0 && req.file.size <= 1048576){
+    // Call function to initate file conversion
     komposeConvert(path, (err, data /* stderr */) => {
       // Check if returned command is successful
       if (err) {
@@ -36,7 +35,7 @@ router.post('/', upload.single('compose_file'), (req, res) => {
       }
     });
     } else {
-      res.status(404).json({ status: 'file is empty'})
+      res.status(404).json({ status: 'file size is invalid'})
     }
   } else {
     res.status(404).json({ status: 'file format invalid' });
