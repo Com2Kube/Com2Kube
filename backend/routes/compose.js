a1016327e3c840fc16c1db48e97e52165ad7a3e8
Gabriel Jean <theelithe555@gmail.com>
2020-01-12 15:29:33 +0000

    Fix lint

--- backend/routes/compose.js
+++ backend/routes/compose.js
@@ -21,21 +21,20 @@ router.post('/', upload.single('compose_file'), (req, res) => {
   // Check if file extention is valid
   if (ext[ext.length - 1] === 'yml' || ext[ext.length - 1] === 'yaml') {
     // Check if file is empty
-    if (req.file.size != 0 && req.file.size <= 1048576){
-    // Call function to initate file conversion
-    komposeConvert(path, (err, data /* stderr */) => {
-      // Check if returned command is successful
-      if (err) {
-        // Send the error
-        res.send(err);
-      } else {
-        // Send the result
-        res.send(data);
-        
-      }
-    });
+    if (req.file.size !== 0 && req.file.size <= 1048576) {
+      // Call function to initate file conversion
+      komposeConvert(path, (err, data /* stderr */) => {
+        // Check if returned command is successful
+        if (err) {
+          // Send the error
+          res.send(err);
+        } else {
+          // Send the result
+          res.send(data);
+        }
+      });
     } else {
-      res.status(404).json({ status: 'file size is invalid'})
+      res.status(404).json({ status: 'file size is invalid' });
     }
   } else {
     res.status(404).json({ status: 'file format invalid' });
