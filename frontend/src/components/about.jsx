e885f76effeeb61bbec28256000066e1a19f8a3f
Claire <clairebun_3@hotmail.com>
2020-01-21 21:59:52 -0500

    about content

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -1,20 +1,32 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
+import { useTranslation } from "react-i18next"
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     with: "100%",
     display: "flex",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    padding: theme.spacing(2)
+  },
+  body: {
+    margin: theme.spacing(5),
+    minWidth: "10%"
   }
 }))
 
 const About = () => {
+  const { t } = useTranslation()
   const classes = useStyles()
   return (
     <div className={classes.root}>
-      <p>About component</p>
+      <div className={classes.body}>
+        <p>{t("about.item1")}</p>
+        <p>{t("about.item2")}</p>
+        <p>{t("about.item3")}</p>
+        <p>{t("about.item4")}</p>
+      </div>
     </div>
   )
 }
