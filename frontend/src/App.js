6694908df440a363dfc6482f8ef6ae9223b74ec0
Gabriel Jean <theelithe555@gmail.com>
2020-01-25 20:49:06 +0000

    Added onsubmit event for the form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-import React, { Suspense } from "react"
+import React, { Suspense, useEffect  } from "react"
 import "./App.css"
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
 import Header from "./themes/header/header"
 import Index from "./pages/index"
 import About from "./pages/about"
 import NotFound from "./pages/not-found"
+import ReactGA from "react-ga"
+
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('UA-156694657-1');
+    // To Report Page View 
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
+  useEffect(() => {
+   console.log(window.location.pathname)
+  })
+
   return (
     <div className="App">
       <Suspense fallback="loading">
