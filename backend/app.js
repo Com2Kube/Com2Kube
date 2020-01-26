3a21fad7c4e564a586890c2da5d2fef0abe02344
Gabriel Jean <theelithe555@gmail.com>
2020-01-26 20:07:38 +0000

    created /api/sample endpoint in the backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+
 const app = express();
 
 // Enable cors
@@ -9,10 +10,12 @@ app.use(cors());
 // Import Routes
 const statusRoute = require('./routes/status');
 const composeRoute = require('./routes/compose');
+const sampleRoute = require('./routes/sample')
 
 // Routes
 app.use('/api/status', statusRoute);
 app.use('/api/compose', composeRoute);
+app.use('/api/sample', sampleRoute);
 
 // Start server
 app.listen(5000);
