a716a5c00ba0a773c154a99e6e5c80df74f07905
Claire <clairebun_3@hotmail.com>
2020-01-25 22:20:08 -0500

    add beta message

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
-import { Box, Container } from "@material-ui/core"
+import { Box, Container, Typography } from "@material-ui/core"
 import { useTranslation } from "react-i18next"
 import FileUpload from "../components/file-upload"
 
@@ -13,8 +13,14 @@ const useStyles = makeStyles(() => ({
     justifyContent: "center"
   },
   text: {
+    alignItems: "center",
     justifyContent: "center",
-    alignItems: "center"
+    textAlign: "center",
+    margin: "auto",
+    padding: "2vh"
+  },
+  li: {
+    listStyle: "none"
   }
 }))
 
@@ -26,11 +32,14 @@ const Index = () => {
       <Container className={classes.text}>
         <p>{t("index.introduction")}</p>
         <p>{t("index.steps")}</p>
-        <ol>
+        <ol className={classes.li}>
           <li>{t("index.step1")}</li>
           <li>{t("index.step2")}</li>
           <li>{t("index.step3")}</li>
         </ol>
+        <Typography variant="h5" color="textSecondary" className={classes.text}>
+          {t("index.betaMessage")}
+        </Typography>
       </Container>
       <Box m="2rem">
         <FileUpload />
