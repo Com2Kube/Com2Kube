a8709173316523ac49113ff78e2bc5f2b69027ee
Gabriel Jean <theelithe555@gmail.com>
2020-02-01 00:35:52 +0000

    fix lint

--- frontend/src/themes/footer.jsx
+++ frontend/src/themes/footer.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import { Typography } from "@material-ui/core"
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
