52341fcc197048583ecaab74e221f9896c3cf24f
Claire <clairebun_3@hotmail.com>
2020-04-12 15:33:37 -0400

    fix unit test for blog

--- frontend/src/__tests__/blog.test.jsx
+++ frontend/src/__tests__/blog.test.jsx
@@ -1,10 +1,18 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import axios from "axios"
 import Blog from "../pages/blog"
 
 describe("Blog", () => {
   test("renders correctly enzyme", () => {
     const wrapper = renderer.create(<Blog />).toJSON()
     expect(wrapper).toMatchSnapshot()
+  })
+  it("fetches successfully data from an API", () => {
+    const data = {
+      title: "DevOps Handy Tool: Com2kube"
+    }
+    expect(data).toBeDefined()
+    expect(data.title).toEqual("DevOps Handy Tool: Com2kube")
   })
 })
