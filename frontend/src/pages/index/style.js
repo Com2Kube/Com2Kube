6c73d2bb7f03cac724d4209225688e816327883d
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-05-21 22:56:40 +0000

    removed comments

--- frontend/src/pages/index/style.js
+++ frontend/src/pages/index/style.js
@@ -2,12 +2,7 @@ import styled from "styled-components"
 
 export const BackgroundImage = styled.img`
     width: 80%;
-    
+
 //   @media (max-width: 400px) {
-//     height: 95px;
-//     width: 105px;
-//     display: flex;
-//     flex-direction: column;
-//     flex-wrap: wrap;
 //   }
 `
