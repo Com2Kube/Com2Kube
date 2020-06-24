0bbea39cc3465fcded6e0547fc9b986ee865b6b5
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2020-06-24 16:26:01 -0400

    center image in the grid

--- frontend/src/pages/index/style.js
+++ frontend/src/pages/index/style.js
@@ -1,7 +1,11 @@
 import styled from "styled-components"
 
 export const BackgroundImage = styled.img`
-    width: 80%;
+    width: 90%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    
 
 //   @media (max-width: 400px) {
 //   }
